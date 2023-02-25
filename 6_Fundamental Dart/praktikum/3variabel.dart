b4bf10eeedd062984a6dd1f44cec4d8e6ad82821
gilanggfzn <gilangm913@gmail.com>
2023-02-25 16:48:09 +0700

    section 6 & 7

---
+++ 6_Fundamental Dart/praktikum/3variabel.dart
@@ -0,0 +1,7 @@
+void main(){
+  var nama="Muhamad Gilang Fauzan";
+  var kota="Bandung";
+  var kampus="Institut Teknologi PLN";
+
+  print("Perkenalkan nama saya $nama asal $kota kampus saya di$kampus");
+}
