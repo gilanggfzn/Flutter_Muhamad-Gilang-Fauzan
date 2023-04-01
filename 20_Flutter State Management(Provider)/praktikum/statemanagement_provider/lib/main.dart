aeed72295e2ba2e633306612ef800847ae35d068
gilanggfzn <gilangm913@gmail.com>
2023-04-02 06:26:10 +0700

    tugas provider dan animation

---
+++ 20_Flutter State Management(Provider)/praktikum/statemanagement_provider/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:page_animation_transition/animations/left_to_right_faded_transition.dart';
+import 'package:page_animation_transition/page_animation_transition.dart';
+import 'package:provider/provider.dart';
+import 'package:statemanagement_provider/page/detail.dart';
+import 'package:statemanagement_provider/page/gridpage.dart';
+import 'package:statemanagement_provider/provider/image_provider.dart';
+
+void main() {
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<PhotoProvider>(
+        create: (_) => PhotoProvider(),
+      )
+    ],
+    child: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      debugShowCheckedModeBanner: false,
+     /* onGenerateRoute: (settings) {
+    switch (settings.name) {
+      case '/detail':
+        return PageAnimationTransition(
+          page: const Detailagens(), 
+          pageAnimationType: LeftToRightFadedTransition());
+
+      default:
+        return null;
+    }
+  },*/
+      routes: {
+        '/': (context) => const HomePage(),
+        '/detail':(context) => const Detailagens(),
+      },
+    );
+  }
+}
---
+++ 21_Flutter State Management(BLoC)/Readme.md
@@ -0,0 +1,8 @@
+# State management BLOC(Business Logic Component)
+BLoc berguna untuk memisahkan antara business logic dengan ui, BLoc digunakan karena dianggap lebih simple, Powerful, dan juga testable,
+didalam BLoc terdapat stream yang merupakan rangkaian proses secara asynchronous dan menjadi actor utama di dalam BLoc
+
+## Cara Kerja BLoc
+* BLoc menerima event sebagai input
+* DIanalisa dan dikelola didalam BLoc
+* Menghasilkan state sebagai output
